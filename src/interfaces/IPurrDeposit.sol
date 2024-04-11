efd68eca70333a1e929509eda8c6e4c434973a5f
SC-Purr <sc@purr.fund>
2024-04-11 17:53:34 +0700

    update(interfaces): IPurrDeposit

--- src/interfaces/IPurrDeposit.sol
+++ src/interfaces/IPurrDeposit.sol
@@ -8,4 +8,35 @@ interface IPurrDeposit {
     // error list
     error InvalidAmount(uint256 amount);
     error InsufficientAllowance();
+
+    /**
+     * @notice Deposit usdc.
+     *
+     * @dev Will transfer usdc to _rootAdmin.
+     * @dev Emit a {Deposit} event.
+     *
+     * Requirements:
+     *   - Require sender approve amount usdc for this contract more than {_amount}.
+     *
+     * @param _amount The amount user will deposit.
+     */
+    function deposit(uint256 _amount) external;
+
+    /**
+     * @notice Set usdc.
+     * @param _usdc The usdc address.
+     */
+    function setUsdc(address _usdc) external;
+
+    /**
+     * @notice Set rootAdmin.
+     * @param rootAdmin_ The rootAdmin address.
+     */
+    function setRootAdmin(address rootAdmin_) external;
+
+    /**
+     * @notice Get rootAdmin.
+     * @return The rootAdmin address.
+     */
+    function getRootAdmin() external returns (address);
 }

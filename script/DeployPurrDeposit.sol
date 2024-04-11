7f675e9843a372e640fc10f551cc02080c9bbff6
SC-Purr <sc@purr.fund>
2024-04-11 17:54:03 +0700

    feat(script): DeployPurrDeposit

---
+++ script/DeployPurrDeposit.sol
@@ -0,0 +1,16 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { BaseScript } from "./Base.s.sol";
+import { PurrDeposit } from "../src/PurrDeposit.sol";
+
+contract DeployMockUSDCScript is BaseScript {
+    address rootAdmin = vm.envAddress("ROOT_ADMIN");
+    // mock usdc
+    address usdc = 0xcB269E7e42D8728C91CCF840c27A25f11285548f; 
+    
+    function run() public broadcast {
+        new PurrDeposit(msg.sender, usdc, rootAdmin);
+    }
+}

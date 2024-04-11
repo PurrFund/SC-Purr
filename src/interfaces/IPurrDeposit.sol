c49be4b93a02a15979bc826ceb5cdd55821dfe9d
SC-Purr <sc@purr.fund>
2024-04-11 16:03:39 +0700

    feat(interfaces): add IPurrDeposit

---
+++ src/interfaces/IPurrDeposit.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identier: MIT
+pragma solidity ^0.8.20;
+
+interface IPurrDeposit {
+    // event list
+    event Deposit(address indexed depositor, address indexed receiver, uint256 amount, uint256 timeDeposit);
+
+    // error list
+    error InvalidAmount(uint256 amount);
+    error InsufficientAllowance();
+}

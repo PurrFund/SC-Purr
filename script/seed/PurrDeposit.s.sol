3c9a2495c5fc55f542d11a465bb8e804298bc87e
SC-Purr <sc@purr.fund>
2024-04-12 09:26:09 +0700

    update(script): PurrDeposit

--- script/seed/PurrDeposit.s.sol
+++ script/seed/PurrDeposit.s.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
@@ -9,16 +8,24 @@ import { MockUSDC } from "../../src/token/MockUSDC.sol";
 contract DeployMockUSDCScript is BaseScript {
     address rootAdmin = vm.envAddress("ROOT_ADMIN");
     // mock usdc
-    address usdc = 0xcB269E7e42D8728C91CCF840c27A25f11285548f; 
-    address[] seedAddress  = [0x9C623EfF30c8BCba288fc0346C44576d3c7FF52C, 0x1405dC6c6cB6Cb9480F01E3E43a5ec89f680Cb8D, 0xA9c80A4ece07EAcA61E20c79c7D4DE343A6A3d27, 0xFFe9F67093c3af3bDB621be78Ed45F5d9CF5200e, 0x93B7CE2fc14e10b40566a76c17d11045aF9AEe85, 0x6a8aaE6a359567BA636CC77CD2592849a0E3e56E, 0xd1024Ee5a24585311F99cbcC18E4F80ad90a04F1, 0x59c2E42B4275Bf84d653a8A5e5b874BED32688a9];
-
+    address usdc = 0xcB269E7e42D8728C91CCF840c27A25f11285548f;
+    address[] seedAddress = [
+        0x9C623EfF30c8BCba288fc0346C44576d3c7FF52C,
+        0x1405dC6c6cB6Cb9480F01E3E43a5ec89f680Cb8D,
+        0xA9c80A4ece07EAcA61E20c79c7D4DE343A6A3d27,
+        0xFFe9F67093c3af3bDB621be78Ed45F5d9CF5200e,
+        0x93B7CE2fc14e10b40566a76c17d11045aF9AEe85,
+        0x6a8aaE6a359567BA636CC77CD2592849a0E3e56E,
+        0xd1024Ee5a24585311F99cbcC18E4F80ad90a04F1,
+        0x59c2E42B4275Bf84d653a8A5e5b874BED32688a9
+    ];
 
     function seedDeposit(address _deposit) public broadcast {
-        for(uint256 i; i< seedAddress.length; i++){
+        for (uint256 i; i < seedAddress.length; i++) {
             MockUSDC(usdc).mint(seedAddress[i], i * 1e18 * 1000);
-            vm.startPrank(seedAddress[i]); 
+            vm.startPrank(seedAddress[i]);
             MockUSDC(usdc).approve(_deposit, i * 1e18 * 1000);
-            PurrDeposit(_deposit).deposit(i*1e18);
+            PurrDeposit(_deposit).deposit(i * 1e18);
             vm.stopPrank();
         }
     }

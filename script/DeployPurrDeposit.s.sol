e04fde6e7faab3f93183eb9dcf493ca2a4f3ad5c
SC-Purr <sc@purr.fund>
2024-04-12 09:24:15 +0700

    update(script): DeployPurrDeposit

--- script/DeployPurrDeposit.s.sol
+++ script/DeployPurrDeposit.s.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.20;
 import { BaseScript } from "./Base.s.sol";
 import { PurrDeposit } from "../src/PurrDeposit.sol";
 
-contract DeployMockUSDCScript is BaseScript {
+contract DeployPurrDepositScript is BaseScript {
     address rootAdmin = vm.envAddress("ROOT_ADMIN");
     // mock usdc
     address usdc = 0xcB269E7e42D8728C91CCF840c27A25f11285548f; 

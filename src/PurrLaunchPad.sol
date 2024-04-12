6189b1b82e42c34058c155fc229edeffd9360c35
SC-Purr <sc@purr.fund>
2024-04-12 17:29:29 +0700

    update(contract): PurrLaunchPad

--- src/PurrLaunchPad.sol
+++ src/PurrLaunchPad.sol
@@ -4,60 +4,55 @@ pragma solidity ^0.8.20;
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { ProjectProfile, Vesting, Project } from "./types/LaunchPadType.sol";
+
+import { LaunchPool, LaunchPad, PreProject, Project } from "./types/LaunchPadType.sol";
 import { IPurrLaunchPad } from "./interfaces/IPurrLaunchPad.sol";
 
+/**
+ * @notice PurrLaunchPad contract.
+ */
 contract PurrLaunchPad is Ownable, IPurrLaunchPad {
     using SafeERC20 for IERC20;
 
     uint64 public projectId;
 
     mapping(uint64 projectId => Project project) public projectInfo;
-    mapping(uint64 projectId => Vesting vesting) public vestingInfo;
-    mapping(uint64 projectId => ProjectProfile profile) public projectProfile;
+    mapping(uint64 projectId => LaunchPool launchPool) public launchPoolInfo;
+    mapping(uint64 projectId => LaunchPad launchPad) public launchPadInfo;
 
     constructor(address initialOwner) Ownable(initialOwner) { }
 
+    /**
+     * @inheritdoc IPurrLaunchPad
+     */
     function createProject(
-        Project memory project,
-        Vesting memory vesting,
-        ProjectProfile memory profile
+        PreProject memory project,
+        LaunchPool memory launchPool,
+        LaunchPad memory launchPad
     )
         external
         onlyOwner
-        returns (bool)
     {
-        _createProject(project, vesting, profile);
-
-        return true;
-    }
-
-    /**
-     * @dev Create new project
-     */
-    function _createProject(
-        Project memory project,
-        Vesting memory vesting,
-        ProjectProfile memory profile
-    )
-        internal
-        returns (bool)
-    {
-        if (vesting.tge <= block.timestamp || vesting.cliffTime <= 0 || vesting.unlockPercent <= 100) {
-            revert InvalidScheduleVesting();
-        }
-
-        if (project.tokenIdoDeciamls <= 0 || project.tokenDecimals <= 0) {
-            revert InvalidDecimal();
-        }
-
         ++projectId;
-        projectInfo[projectId] = project;
-        vestingInfo[projectId] = vesting;
-        projectProfile[projectId] = profile;
+        projectInfo[projectId] = Project({
+            id: projectId,
+            owner: project.owner,
+            tokenIDO: project.tokenIDO,
+            tokenUseToBuy: project.tokenUseToBuy,
+            isRequireKyc: project.isRequireKyc,
+            isRequireStake: project.isRequireStake,
+            isRequrieVesting: project.isRequrieVesting,
+            useWhitelist: project.useWhitelist,
+            name: project.name,
+            twitter: project.twitter,
+            discord: project.discord,
+            telegram: project.telegram,
+            website: project.website
+        });
 
-        emit CreateProject(projectId, project, vesting, profile);
+        launchPoolInfo[projectId] = launchPool;
+        launchPadInfo[projectId] = launchPad;
 
-        return true;
+        emit CreateProject(projectInfo[projectId], launchPad, launchPool);
     }
 }

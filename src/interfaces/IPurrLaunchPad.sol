9267ccebbe6609f724d72f8ddb48f88e2b48482d
SC-Purr <sc@purr.fund>
2024-04-12 17:30:37 +0700

    update(interfaces): udpate IPurrLaunchPad

--- src/interfaces/IPurrLaunchPad.sol
+++ src/interfaces/IPurrLaunchPad.sol
@@ -2,13 +2,24 @@
 
 pragma solidity ^0.8.20;
 
-import { ProjectProfile, Vesting, Project } from "../types/LaunchPadType.sol";
+import { LaunchPool, LaunchPad, PreProject, Project } from "../types/LaunchPadType.sol";
 
 interface IPurrLaunchPad {
     // event list
-    event CreateProject(uint256 indexed projectId, Project project, Vesting vesting, ProjectProfile profile);
+    event CreateProject(Project project, LaunchPad launchPad, LaunchPool launchPool);
 
-    // error list
-    error InvalidScheduleVesting();
-    error InvalidDecimal();
+    /**
+     * @notice Create new project.
+     *
+     * @dev Emit a {CreateProject} event.
+     *
+     * Requirements:
+     * - Require onwner role
+     *
+     * @param project The project info.
+     * @param launchPool The launch pool info.
+     * @param launchPad The launch pad info.
+     *
+     */
+    function createProject(PreProject memory project, LaunchPool memory launchPool, LaunchPad memory launchPad) external;
 }

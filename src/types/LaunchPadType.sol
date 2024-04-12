71c8a9ce6551b58e4ea9f3e9acd8968b92a0fcdd
SC-Purr <sc@purr.fund>
2024-04-12 17:31:39 +0700

    update(types): LaunchPadType

--- src/types/LaunchPadType.sol
+++ src/types/LaunchPadType.sol
@@ -1,9 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-library LaunchPad { }
-
-struct ProjectProfile {
+struct Project {
+    uint64 id;
+    address owner;
+    address tokenIDO;
+    address tokenUseToBuy;
+    bool isRequireKyc;
+    bool isRequireStake;
+    bool isRequrieVesting;
+    bool useWhitelist;
     string name;
     string twitter;
     string discord;
@@ -11,84 +17,45 @@ struct ProjectProfile {
     string website;
 }
 
-struct Order {
-    address buyer;
-    uint256 coinAmount;
-    uint256 tokenAmount;
-    uint256 tokenReleased;
-}
-
-struct LaunchPadState {
-    RoundType round;
-    uint64 participants;
-    uint128 startTime;
-    uint128 endTime;
-    uint256 softCap;
-    uint256 hardCap;
-    uint256 totalTokenSold;
-    uint256 swapRatioCoin;
-    uint256 swapRatioToken;
-    //when project stop fund-raising, to claim or refund
-    // token_fund: Coin<TOKEN>,
-    uint256 totalTokenDeposited;
-    uint256 totalCoinedRaised;
-    uint256 defaultMaxAllocate;
-    RoundState state;
-}
-// coin_raised: Coin<COIN>,
-// order_book: Table<address, Order>,
-// uint256 default_max_allocate;
-// max_allocations: Table<address, u64>,
-
-struct Vesting {
-    VestingType _type;
-    uint256 tge;
-    uint256 cliffTime;
-    //cliff time duration in ms
-    uint256 unlockPercent;
-    //unlock percent scaled to x10
-    uint256 linearTime;
-    //linear vesting duration if linear mode
-    uint256[] time;
-    uint256[] percent;
-}
-
-struct Project {
-    uint64 id;
+struct PreProject {
     address owner;
-    uint8 tokenIdoDeciamls;
-    uint8 tokenDecimals;
-    // address[] whitelist;
     address tokenIDO;
     address tokenUseToBuy;
     bool isRequireKyc;
     bool isRequireStake;
     bool isRequrieVesting;
-    bool isByBuyNative;
     bool useWhitelist;
-}
-// FeeType feeType;
-// ListType listType; // if manual : presale, privatesale, seed sale
-
-struct SetUpProject {
-    RoundType round;
-    uint64 projectId;
-    uint128 startTime;
-    uint128 endTime;
-    bool useWhitelist;
-    uint256 swapRatioCoin;
-    uint256 swapRatioToken;
-    uint256 maxAllocate;
-    uint256 softCap;
-    uint256 hardCap;
+    string name;
+    string twitter;
+    string discord;
+    string telegram;
+    string website;
 }
 
-enum RoundState {
-    ROUND_STATE_INIT,
-    ROUND_STATE_PREPARE,
-    ROUND_STATE_RASING,
-    ROUND_STATE_REFUNDING,
-    ROUND_STATE_CLAIMING
+struct LaunchPool {
+    VestingType typeVesting;
+    uint256 tge;
+    uint256 cliffTime;
+    uint256 unlockPercent;
+    uint256 linearTime;
+    uint256[] time;
+    uint256[] percent;
+    uint256 tokenOffer;
+    uint256 tokenPrice;
+    uint256 totalRaise;
+    uint256 ticketSize;
+}
+
+struct LaunchPad {
+    VestingType typeVesting;
+    uint256 tge;
+    uint256 cliffTime;
+    uint256 unlockPercent;
+    uint256 linearTime;
+    uint256[] time;
+    uint256[] percent;
+    uint256 tokenReward;
+    uint256 totalAirdrop;
 }
 
 enum VestingType {
@@ -96,21 +63,4 @@ enum VestingType {
     VESTING_TYPE_MILESTONE_UNLOCK_FIRST,
     VESTING_TYPE_LINEAR_UNLOCK_FIRST,
     VESTING_TYPE_LINEAR_CLIFF_FIRST
-}
-
-enum FeeType {
-    FIX,
-    PERCENT
-}
-
-enum ListType {
-    Auto,
-    Manual
-}
-
-enum RoundType {
-    ROUND_NONE,
-    ROUND_SEED,
-    ROUND_PRIVATE,
-    ROUND_PUBLIC
 }

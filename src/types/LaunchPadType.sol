7135e7eee049219f8edac7bc358d60b34d9a32cc
SC-Purr <sc@purr.fund>
2024-04-15 18:37:32 +0700

    update(type): LaunchPadType

--- src/types/LaunchPadType.sol
+++ src/types/LaunchPadType.sol
@@ -5,11 +5,6 @@ struct Project {
     uint64 id;
     address owner;
     address tokenIDO;
-    address tokenUseToBuy;
-    bool isRequireKyc;
-    bool isRequireStake;
-    bool isRequrieVesting;
-    bool useWhitelist;
     string name;
     string twitter;
     string discord;
@@ -20,11 +15,6 @@ struct Project {
 struct PreProject {
     address owner;
     address tokenIDO;
-    address tokenUseToBuy;
-    bool isRequireKyc;
-    bool isRequireStake;
-    bool isRequrieVesting;
-    bool useWhitelist;
     string name;
     string twitter;
     string discord;
@@ -33,29 +23,31 @@ struct PreProject {
 }
 
 struct LaunchPool {
-    VestingType typeVesting;
+    uint16 unlockPercent;
+    uint64 snapshotTime;
+    uint16[] percents;
+    uint64[] times;
     uint256 tge;
     uint256 cliffTime;
-    uint256 unlockPercent;
     uint256 linearTime;
-    uint256[] time;
-    uint256[] percent;
     uint256 tokenOffer;
     uint256 tokenPrice;
     uint256 totalRaise;
     uint256 ticketSize;
+    VestingType typeVesting;
 }
 
 struct LaunchPad {
-    VestingType typeVesting;
+    uint16 unlockPercent;
+    uint64 snapshotTime;
+    uint16[] percents;
+    uint64[] times;
     uint256 tge;
     uint256 cliffTime;
-    uint256 unlockPercent;
     uint256 linearTime;
-    uint256[] time;
-    uint256[] percent;
     uint256 tokenReward;
     uint256 totalAirdrop;
+    VestingType typeVesting;
 }
 
 enum VestingType {

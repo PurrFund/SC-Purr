b2c4ed8f3fdcfbb83f31521447f2be39744d1d6f
SC-Purr <sc@purr.fund>
2024-04-15 18:35:56 +0700

    update(contract): PurrStaking

--- src/PurrStaking.sol
+++ src/PurrStaking.sol
@@ -6,7 +6,7 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
 
-import { UserPower, PoolInfo, PoolType } from "./types/PurrStaingType.sol";
+import { UserPoolInfo, PoolInfo, PoolType } from "./types/PurrStaingType.sol";
 import { IPurrStaking } from "./interfaces/IPurrStaking.sol";
 
 /**
@@ -17,15 +17,19 @@ contract PurrStaking is IPurrStaking, Ownable {
     using SafeERC20 for IERC20;
     using Math for uint256;
 
+    uint256 public immutable SECOND_YEAR;
+
     uint16 public poolId;
-    uint256 public timeLock;
+    uint256 public itemId;
+
     IERC20 public launchPadToken;
 
-    mapping(address staker => UserPower userPower) public userPower;
     mapping(PoolType poolType => PoolInfo pool) public poolInfo;
+    mapping(uint256 itemId => UserPoolInfo userPool) public userPoolInfo;
 
     constructor(address _launchPadToken, address _initialOnwer) Ownable(_initialOnwer) {
         launchPadToken = IERC20(_launchPadToken);
+        SECOND_YEAR = 31_536_000;
     }
 
     /**
@@ -43,6 +47,14 @@ contract PurrStaking is IPurrStaking, Ownable {
         _stake(_amount, _poolType);
     }
 
+    function getPendingReward(uint256 _itemId) external returns (uint256) {
+        return _calculatePendingReward(_itemId);
+    }
+
+    function claimReward(uint256 _itemId) external {
+        _claimReward(_itemId);
+    }
+
     /**
      * @notice Unstake token's protocol.
      *
@@ -57,18 +69,18 @@ contract PurrStaking is IPurrStaking, Ownable {
      *
      * @return status The result of staking.
      */
-    function unstake(uint256 _amount, PoolType _poolType) external returns (bool) {
-        bool status = _unStake(_amount, _poolType);
-        return status;
-    }
+    // function unstake(uint256 _amount, uint256 _itemId) external returns (bool) {
+    //     bool status = _unStake(_amount, _poolType);
+    //     return status;
+    // }
 
     // function withDraw() extenal return (bool) {
     //     _withDraw();
     // }
 
-    function updatePool(PoolType _poolType, PoolInfo calldata pool) external onlyOwner {
-        poolInfo[_poolType] = pool;
-    }
+    // function updatePool(PoolType _poolType, PoolInfo calldata pool) external onlyOwner {
+    //     poolInfo[_poolType] = pool;
+    // }
 
     /**
      * @dev Equivalent to {stake} function.
@@ -76,36 +88,33 @@ contract PurrStaking is IPurrStaking, Ownable {
     function _stake(uint256 _amount, PoolType _poolType) internal returns (bool) {
         address sender = msg.sender;
         PoolInfo storage pool = poolInfo[_poolType];
-        UserPower storage user = userPower[sender];
-        uint256 currentPoint = _amount * pool.multiplier;
+        uint256 point = _amount * pool.multiplier;
 
         if (launchPadToken.balanceOf(sender) < _amount) {
             revert InsufficientAmount(_amount);
-        }
-
-        if (currentPoint < pool.minPoint) {
-            revert InvalidPoint(currentPoint);
         }
 
         if (_amount < 0) {
             revert InvalidAmount(_amount);
         }
 
-        // update pool weight
         pool.totalStaked += _amount;
         ++pool.numberStaker;
 
-        // update user powerun
-        user.balance += _amount;
-        user.pPoint += currentPoint;
-
-        (PoolType tier, uint256 userWeight) = _getPower(user.pPoint);
-        user.tier = tier;
-        user.weight = userWeight;
+        // update user pool infor
+        ++itemId;
+        userPoolInfo[itemId] = UserPoolInfo({
+            staker: msg.sender,
+            pPoint: point,
+            stakedAmount: _amount,
+            start: block.timestamp,
+            end: block.timestamp + pool.lockDay,
+            poolType: _poolType
+        });
 
         IERC20(launchPadToken).safeTransferFrom(sender, address(this), _amount);
 
-        emit Stake(sender, _amount, block.timestamp, currentPoint, userWeight, tier);
+        emit Stake(sender, itemId, _amount, point, block.timestamp, block.timestamp + pool.lockDay, _poolType);
 
         return true;
     }
@@ -113,66 +122,96 @@ contract PurrStaking is IPurrStaking, Ownable {
     /**
      * @dev Equivalent to {unStake} function.
      */
-    function _unStake(uint256 _amount, PoolType _poolType) internal returns (bool) {
-        address sender = msg.sender;
-        PoolInfo storage pool = poolInfo[_poolType];
-        UserPower storage user = userPower[sender];
-        uint256 currentBalance = user.balance;
-        uint256 currentPoint = _amount * pool.multiplier;
-        uint256 timeStake = block.timestamp - user.timeLocked;
+    // function _unStake(uint256 _amount, uint256 _itemId) internal returns (bool) {
+    //     address sender = msg.sender;
+    //     PoolInfo storage pool = poolInfo[_poolType];
+    //     UserPoolInfo storage userPooluser = userPoolInfo[itemId];
+    //     uint256 point = _amount * pool.multiplier;
+    //     uint256 currentBalance = userPooluser.stakedAmount;
+    //     uint256 currentPoint = userPooluser.pPoint;
+    //     uint256 timeStake = block.timestamp - user.timeLocked;
 
-        if (_amount > currentBalance) {
-            revert ExceedBalance(_amount);
-        }
+    //     if (_amount > currentBalance) {
+    //         revert ExceedBalance(_amount);
+    //     }
 
-        if (_amount == 0) {
-            revert InvalidAmount(_amount);
-        }
+    //     if (_amount == 0) {
+    //         revert InvalidAmount(_amount);
+    //     }
 
-        if (timeStake < pool.lockPeriodInDays) {
-            // later handle
-        }
+    //     // if(pool.poolType = PoolType.) {
 
-        pool.totalStaked -= _amount;
-        if (_amount == currentBalance) {
-            --pool.numberStaker;
-        }
+    //     // }
 
-        currentBalance -= _amount;
-        user.pPoint -= currentPoint;
-        user.balance = currentBalance;
+    //     if (timeStake < pool.lockPeriodInDays) {
+    //         // later handle
+    //     }
 
-        (PoolType tier, uint256 userWeight) = _getPower(currentBalance);
-        user.tier = tier;
-        user.weight = userWeight;
+    //     pool.totalStaked -= _amount;
+    //     if (_amount == currentBalance) {
+    //         --pool.numberStaker;
+    //     }
 
-        IERC20(launchPadToken).safeTransferFrom(msg.sender, address(this), _amount);
+    //     currentBalance -= _amount;
+    //     user.pPoint -= currentPoint;
+    //     user.balance = currentBalance;
 
-        emit UnStake(sender, _amount, block.timestamp, user.pPoint, userWeight, tier);
+    //     (PoolType tier, uint256 userWeight) = _getPower(currentBalance);
+    //     user.tier = tier;
+    //     user.weight = userWeight;
 
-        return true;
-    }
+    //     IERC20(launchPadToken).safeTransferFrom(msg.sender, address(this), _amount);
+
+    //     emit UnStake(sender, _amount, block.timestamp, user.pPoint, userWeight, tier);
+
+    // }
 
     /**
      * @dev Get user's power base on user's balance
      */
-    function _getPower(uint256 _point) internal view returns (PoolType tier, uint256 weight) {
-        if (_point < poolInfo[PoolType.BZONZE].minPoint) {
-            return (PoolType.ZERO, _point);
-        } else if (_point < poolInfo[PoolType.SLIVER].minPoint) {
-            return (PoolType.BZONZE, _getWeight(_point, poolInfo[PoolType.BZONZE].minPoint, poolInfo[PoolType.BZONZE].minWeight));
-        } else if (_point < poolInfo[PoolType.GOLD].minPoint) {
-            return (PoolType.SLIVER, _getWeight(_point, poolInfo[PoolType.SLIVER].minPoint, poolInfo[PoolType.SLIVER].minWeight));
-        } else if (_point < poolInfo[PoolType.DIAMOND].minPoint) {
-            return (PoolType.GOLD, _getWeight(_point, poolInfo[PoolType.GOLD].minPoint, poolInfo[PoolType.GOLD].minWeight));
-        } else if (_point >= poolInfo[PoolType.DIAMOND].minPoint) {
-            return
-                (PoolType.DIAMOND, _getWeight(_point, poolInfo[PoolType.DIAMOND].minPoint, poolInfo[PoolType.DIAMOND].minWeight));
+    // function _getPower(uint256 staker) internal view returns (PoolType tier, uint256 weight) {
+    //     userPoolInfo[msg.sender][]
+    //     if (_point < poolInfo[PoolType.BZONZE].minPoint) {
+    //         return (PoolType.ZERO, _point);
+    //     } else if (_point < poolInfo[PoolType.SLIVER].minPoint) {
+    //         return (PoolType.BZONZE, _getWeight(_point, poolInfo[PoolType.BZONZE].minPoint,
+    // poolInfo[PoolType.BZONZE].minWeight));
+    //     } else if (_point < poolInfo[PoolType.GOLD].minPoint) {
+    //         return (PoolType.SLIVER, _getWeight(_point, poolInfo[PoolType.SLIVER].minPoint,
+    // poolInfo[PoolType.SLIVER].minWeight));
+    //     } else if (_point < poolInfo[PoolType.DIAMOND].minPoint) {
+    //         return (PoolType.GOLD, _getWeight(_point, poolInfo[PoolType.GOLD].minPoint, poolInfo[PoolType.GOLD].minWeight));
+    //     } else if (_point >= poolInfo[PoolType.DIAMOND].minPoint) {
+    //         return
+    //             (PoolType.DIAMOND, _getWeight(_point, poolInfo[PoolType.DIAMOND].minPoint,
+    // poolInfo[PoolType.DIAMOND].minWeight));
+    //     }
+    // }
+
+    // function _getWeight(uint256 _point, uint256 _mintPoint, uint256 _minWeight) internal pure returns (uint256) {
+    //     return _point.mulDiv(_mintPoint, _minWeight, Math.Rounding.Floor);
+    // }
+
+    function _claimReward(uint256 _itemId) internal {
+        if (msg.sender != userPoolInfo[_itemId].staker) {
+            revert InvalidStaker(msg.sender);
         }
+        uint256 reward = _calculatePendingReward(_itemId);
+
+        if (launchPadToken.balanceOf(address(this)) < reward) {
+            revert InsufficientBalance(launchPadToken.balanceOf(address(this)));
+        }
+
+        IERC20(launchPadToken).safeTransferFrom(address(this), msg.sender, reward);
     }
 
-    function _getWeight(uint256 _point, uint256 _mintPoint, uint256 _minWeight) internal pure returns (uint256) {
-        return _point.mulDiv(_mintPoint, _minWeight, Math.Rounding.Floor);
+    function _calculatePendingReward(uint256 _itemId) internal view returns (uint256) {
+        UserPoolInfo memory userPool = userPoolInfo[_itemId];
+        PoolInfo memory pool = poolInfo[userPool.poolType];
+        uint256 timeStaked = block.timestamp - userPool.start;
+        uint256 timeStakedMulApr = timeStaked * pool.apr;
+
+        return userPool.stakedAmount.mulDiv(timeStakedMulApr, SECOND_YEAR, Math.Rounding.Floor);
     }
 
     function _beforeWithDraw() internal { }

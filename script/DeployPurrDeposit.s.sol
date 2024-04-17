4926eab35bf10b46c9d08ca764d6d07d4296ae70
SC-Purr <sc@purr.fund>
2024-04-17 15:20:28 +0700

    update(script): DeployPurrDeposit.s.sol

--- script/DeployPurrDeposit.s.sol
+++ script/DeployPurrDeposit.s.sol
@@ -6,10 +6,11 @@ import { PurrDeposit } from "../src/PurrDeposit.sol";
 
 contract DeployPurrDepositScript is BaseScript {
     address rootAdmin = vm.envAddress("ROOT_ADMIN");
+    address subAdmin = vm.envAddress("SUB_ADMIN");
     // mock usdc
     address usdc = 0xcB269E7e42D8728C91CCF840c27A25f11285548f;
 
     function run() public broadcast {
-        new PurrDeposit(msg.sender, usdc, rootAdmin);
+        new PurrDeposit(msg.sender, usdc, rootAdmin, subAdmin);
     }
 }

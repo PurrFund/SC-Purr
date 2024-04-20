5c1f3964115ce0db57b6f87f5e2d52da2a4974c0
SC-Purr <sc@purr.fund>
2024-04-20 14:24:25 +0700

    update(interfaces): PurrDeposit.t.sol

--- src/interfaces/IPurrDeposit.sol
+++ src/interfaces/IPurrDeposit.sol
@@ -4,82 +4,116 @@ pragma solidity ^0.8.20;
 interface IPurrDeposit {
     // event list
     event Deposit(address indexed depositor, uint256 amount, uint256 timeDeposit);
-    event AddFund(address indexed admin, address indexed receiver, uint256 amount);
-    event WithDraw(address indexed sender, address indexed receiver, uint256 amount);
-    event UpdatePoolDeposit(bool canWithDraw);
-    event SetUsd(address usd);
-    event UpdateRootAdmin(address rootAdmin);
+    event AddFund(address indexed sender, address indexed receiver, uint256 amount);
     event WithDrawRootAdmin(address indexed sender, address indexed receiver, uint256 amount);
-    event UpdateBalanceDepositor();
     event WithDrawUser(address indexed sender, uint256 amount, uint256 timeWithDraw);
 
     // error list
-    error InsufficientAllowance();
     error InsufficientBalance(uint256 amount);
     error InsufficientTotalSupply(uint256 amount);
     error InvalidAmount(uint256 amount);
     error InvalidSubAdmin(address subAdmin);
     error InvalidRootAdmin(address rootAdmin);
-    error InvalidAdmin(address admin);
     error CanNotWithDraw();
     error InvalidArgument();
+    error InvalidUpdateAmount(address depositor, uint256 amount);
 
     /**
-     * @notice Deposit usdc.
+     * @notice Deposit usd.
      *
-     * @dev Will transfer usdc to _rootAdmin.
+     * @dev Will transfer usd to _rootAdmin.
      * @dev Emit a {Deposit} event.
      *
      * Requirements:
-     *   - Require sender approve amount usdc for this contract more than {_amount}.
+     *   - Require sender approve usd for this contract more than {_amount}.
      *
      * @param _amount The amount user will deposit.
      */
     function deposit(uint256 _amount) external;
 
     /**
-     * @notice Set usdc.
-     * @param _usdc The usdc address.
-     */
-    function setUsd(address _usdc) external;
-
-    /**
-     * @notice Set rootAdmin.
-     * @param rootAdmin_ The rootAdmin address.
-     */
-    function setRootAdmin(address rootAdmin_) external;
-
-    /**
      * @notice Add fund to contract.
-     * @param _amount The amount add to contract.
+     *
+     * @dev Will transfer usd to this contract to allow user withdraw their investment amount.
+     * @dev Emit a {AddFund} event.
+     *
+     * Requirements:
+     *  - Require sender approve usd for this contract.
+     *
+     * @param _amount The amount add fund.
      */
     function addFund(uint256 _amount) external;
 
     /**
-     * @notice With draw to owner.
-     * @param _amount The amount to withdraw.
+     * @notice Withdraw usd to rootadmin.
+     *
+     * @dev Will transfer usd from this contract to rootAdmin.
+     * @dev Only rootAdmin can withdraw for secure purpose.
+     * @dev Emit a {WithDrawRootAdmin} event.
+     *
+     * @param _amount The amount add fund.
      */
     function withDrawRootAdmin(uint256 _amount) external;
 
     /**
-     * @notice With draw fund to user.
-     * @param _amount The amount to withdraw.
+     * @notice Withdraw usd from this contract to sender.
+     *
+     * @dev Will transfer usd to this contract to sender.
+     * @dev User can't not withdraw before snapshot time end.
+     * @dev Emit a {WithDrawUser} event.
+     *
+     * @param _amount The amount add fund.
      */
     function withDrawUser(uint256 _amount) external;
 
     /**
-     * @notice Turn off with draw.
+     * @notice Update user balance in contract.
+     *
+     * @dev We will update user's balance after snapshot that they register.
+     * @dev Remain user's balanace based on our calculation in snapshot time.
+     * @dev Only owner can call this function.
+     *
+     * @param _depositorAddresses The array address.
+     * @param  _amounts The array amount.
+     */
+    function updateBalanceDepositor(address[] calldata _depositorAddresses, uint256[] calldata _amounts) external;
+
+    /**
+     * @notice Turn off withdraw.
+     *
+     * @dev We will turn off withdraw mode in snapshot time.
+     * @dev User can withdraw when snapshot time end.
+     * @dev Only subadmin can call this function.
      */
     function turnOffWithDraw() external;
 
     /**
-     * @notice Turn off with draw.
+     * @notice Update withdraw status.
+     *
+     * @dev We will update withdraw status.
+     * @dev Only owner can call this function.
+     *
+     * @param _canWithDraw The _canWithDraw staus.
      */
     function updateStatusWithDraw(bool _canWithDraw) external;
+
     /**
-     * @notice Update depositor's amount deposit in contract.
+     * @notice Update usd address.
      *
-     * @dev Emit {}
+     * @dev We will update usd address.
+     * @dev Only owner can call this function.
+     *
+     * @param _usd The usd address.
      */
-    function updateBalanceDepositor(address[] calldata depositorAddresses, uint256[] calldata amounts) external;
+    function setUsd(address _usd) external;
+
+    /**
+     * @notice Update rootadmin address.
+     *
+     * @dev We will update rootadmin address.
+     * @dev Only rootadmin can call this function.
+     *
+     * @param rootAdmin_ The rootAdmin address.
+     */
+    function setRootAdmin(address rootAdmin_) external;
 }

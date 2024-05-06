abe74408441e8c447fcf86cc2154c6c90c865db9
SC-Purr <sc@purr.fund>
2024-05-06 18:16:07 +0700

    update(contract): vesting

--- src/types/PurrVestingType.sol
+++ src/types/PurrVestingType.sol
@@ -2,31 +2,51 @@
 pragma solidity ^0.8.20;
 
 enum PoolState {
-    NEW,
+    INIT,
     STARTING,
     PAUSE,
-    SUCCESS
+    END
 }
 
 struct Pool {
-    address tokenFund;
     uint256 id;
-    string name;
-    uint8 vestingType;
     uint256 tge;
     uint256 cliff;
     uint256 unlockPercent;
     uint256 linearVestingDuration;
-    uint256[] milestoneTimes;
-    uint256[] milestonePercents;
-    //     mapping(address => uint256) funds;
-    //     mapping(address => uint256) released;
+    uint256[] times;
+    uint256[] percens;
     uint256 fundsTotal;
     uint256 fundsClaimed;
+    address tokenFund;
+    string name;
+    VestingType vestingType;
+    PoolState state;
+}
+
+struct CreatePool {
+    uint256 tge;
+    uint256 cliff;
+    uint256 unlockPercent;
+    uint256 linearVestingDuration;
+    uint256[] times;
+    uint256[] percens;
+    uint256 fundsTotal;
+    uint256 fundsClaimed;
+    address tokenFund;
+    string name;
+    VestingType vestingType;
     PoolState state;
 }
 
 struct UserPool {
     uint256 fund;
     uint256 released;
+}
+
+enum VestingType {
+    VESTING_TYPE_MILESTONE_CLIFF_FIRST,
+    VESTING_TYPE_MILESTONE_UNLOCK_FIRST,
+    VESTING_TYPE_LINEAR_UNLOCK_FIRST,
+    VESTING_TYPE_LINEAR_CLIFF_FIRST
 }

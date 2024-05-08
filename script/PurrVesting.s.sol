ff28235cb5b5d94ad9bda7a8154b7ff3a6bcab2e
SC-Purr <sc@purr.fund>
2024-05-08 18:16:28 +0700

    update(contract): PurrVesting.t.sol

--- script/PurrVesting.s.sol
+++ script/PurrVesting.s.sol
@@ -12,17 +12,18 @@ contract PurrVestingScript is BaseScript {
     address idoMock = 0x9ab93Fe22e82dC098dA674818a74901EB7a9D7A6;
     uint64[] times;
     uint16[] percents;
-    uint256[] fundAmounts;
-    address[] users;
+    uint256[] fundAmountList;
+    address[] userList;
 
     function run() public broadcast {
         purrVesting = new PurrVesting(msg.sender);
         CreatePool memory createPool1 = CreatePool({
+            projectId: "17aa0f02-6ce1-4352-84ab-42bc0fa66d15",
             tokenFund: idoMock,
             name: "FANX",
             vestingType: VestingType.VESTING_TYPE_LINEAR_CLIFF_FIRST,
-            tge: block.timestamp + 1 days,
-            cliff: block.timestamp + 2 days,
+            tge: block.timestamp + 10 seconds,
+            cliff: 10 seconds,
             unlockPercent: 1000,
             linearVestingDuration: 365 days,
             times: times,
@@ -31,15 +32,21 @@ contract PurrVestingScript is BaseScript {
 
         purrVesting.createPool(createPool1);
 
-        ERC20(idoMock).approve(address(purrVesting), 10_000_000_000_000_000_000_000_000);
+        ERC20(idoMock).approve(address(purrVesting), ERC20(idoMock).balanceOf(msg.sender));
 
-        users.push(address(1));
-        users.push(address(2));
-        users.push(address(3));
-        fundAmounts.push(100e18);
-        fundAmounts.push(150e18);
-        fundAmounts.push(120e18);
+        userList.push(0x9C623EfF30c8BCba288fc0346C44576d3c7FF52C);
+        userList.push(0x1405dC6c6cB6Cb9480F01E3E43a5ec89f680Cb8D);
+        userList.push(0xA9c80A4ece07EAcA61E20c79c7D4DE343A6A3d27);
+        fundAmountList.push(100e18);
+        fundAmountList.push(150e18);
+        fundAmountList.push(120e18);
 
-        purrVesting.addFund(1, fundAmounts, users);
+        purrVesting.addFund(1, fundAmountList, userList);
+        purrVesting.start(1);
     }
+
+    // function claim(address _purrVesting, address _user) public broadcastCustom(_user){
+    //     uint256 poolId = 1;
+    //     PurrVesting(_purrVesting).claimFund(_poolId);
+    // }
 }
